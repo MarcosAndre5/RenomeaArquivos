7e33444c63ad136dd7222e45878c30b1a64832e3
Marcos André Azevedo de Assis <marcosandreazevedo@hotmail.com>
2018-10-10 12:45:18 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,26 @@
 # -*- coding: utf-8 -*-
+'''
+27/09/18 - Criação.
+09/10/18 - Atualizado para não renomear os arquivos caso algum deles já esteja com seu nome renomeado.
+Marcos.
+Versão do Python: 3.7.
+'''
 import os
-import shutil
-
 pre = 'iedf200.bco001.'
 pos = '.xjucern.c093757'
-
-
+cont = 0
 for i in os.listdir('.'):
-    if i.startswith('IED'):
-        os.rename(i, i[12:])
-
-[os.rename(i, 'iedf200.bco001.' + str(i)) for i in os.listdir('.') 
-if ((not i.startswith('.')) and i.endswith(".ret"))]
-
-for i in os.listdir('.'):
-    if i.startswith('ied'):
-        os.rename(i, i[:-3])
-
-for i in os.listdir('.'):
-    if i.startswith('ied'):
-        os.rename(i, i + pos)
-
-
-#shutil.copyfile('Users/Suporte/Desktop/deve', os.path.join(arquivo,'Users/Suporte/Desktop/destino')
-            
-# pre = 'iedf200.bco001.'
-# pos = '.xjucern.c093757'
+    if i.startswith('iedf200.bco001.'):
+        cont += 1       
+if(cont == 0):
+    for i in os.listdir('.'):
+        if i.startswith('IED'):
+            os.rename(i, i[12:])
+    [os.rename(i, pre + str(i)) for i in os.listdir('.')
+     if ((not i.startswith('.')) and i.endswith('.ret'))]
+    for i in os.listdir('.'):
+        if i.startswith('ied'):
+            os.rename(i, i[:-3])
+    for i in os.listdir('.'):
+        if i.startswith('ied'):
+            os.rename(i, i + pos)
